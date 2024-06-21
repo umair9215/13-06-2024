5212f88d084fedfdf093b9db5b5b62c8b8ef391f
Malik Muhammad Umair <167450073+umair9215@users.noreply.github.com>
2024-06-21 11:54:06 -0700

    Array

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+// let cars = ["Prius", "Morris", "Alto", "Honda", "Suzuki"];
+// console.log(cars);
+// console.log(cars.indexOf("Alto"));
+
+
+// let fruits = [];
+// fruits[0] = "Apple";
+// fruits[1] = "Banana";
+// fruits[2] = "Grapes";
+// fruits[3] = "Mango";
+// // fruits.pop();
+
+// fruits.push("Carrot");
+// console.log(fruits);
+
+
+let cars = ["Prius", "Morris", "Alto", "Honda", "Suzuki"];
+// cars.shift();
+// console.log(cars);
+cars.unshift("Civic");
+console.log(cars);
